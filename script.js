ce466f22cfba483c85452e93bf215a1e167e301e
hussain-ahusan <hussainahusan902@hotmail.com>
2024-01-13 19:13:39 +0500

    Added UI to game

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+const roundResult = document.querySelector('#result')
+const userPoints = document.querySelector('#player-score')
+const computerPoints = document.querySelector('#computer-score')
+
 const choiceBtns = document.querySelectorAll('button')
 choiceBtns.forEach(button => button.addEventListener('click', (e) => {
-    console.log(checkWinner(button.id, computerPlay()))
+    const result = playRound(button.id, computerPlay())
+    roundResult.textContent = result
+    console.log(checkWinner())
 }))
 
 function computerPlay () {
@@ -9,30 +15,38 @@ function computerPlay () {
     return randomChoice
 }
 
-function checkWinner (player, computer) {
+let playerScore = 0
+let computerScore = 0
+function playRound (player, computer) {
     if (
         (player === "rock" && computer === "scissor") ||
         (player === "scissor" && computer === "paper") ||
         (player === "paper" && computer === "rock")
     ) {
+        playerScore++
+        userPoints.textContent = `Player score is ${playerScore}`
         return `You won! ${player} beats ${computer}`
     } else if (
         (computer === "rock" && player === "scissor") ||
         (computer === "scissor" && player === "paper") ||
         (computer === "paper" && player === "rock")
     ) {
+        computerScore++
+        computerPoints.textContent = `Computer score is: ${computerScore}`
         return `You lose! ${computer} beats ${player}`
     } else {
         return `It's a draw!`
     }
 }
 
-function roundResult (player, computer) {
-    const result = checkWinner(player, computer)
-
-    if (result === `You won! ${player} beats ${computer}`) {
-        return playerScore++
-    } else if (result === `You lose! ${computer} beats ${player}`) {
-        return computerScore++
+function checkWinner () {
+    let player = playerScore
+    let computer = computerScore
+    if (player === computer) {
+        console.log('It\'s a draw!')
+    } else if (player > computer) {
+        console.log(`Player is the winner!`)
+    } else {
+        console.log('Computer is the winner!')
     }
 }
